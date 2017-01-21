82491d7bcb9d4586d94a01287110d277cd828033
FelipeFG92 <felipecomp2012@gmail.com>
2017-01-20 22:19:37 -0200

    Codes used in the initial tests

---
+++ Initial tests code/LightSensor/lightSensorTest.ino
@@ -0,0 +1,16 @@
+//Using the Serial Monitor, this program enables the user to determine a threshold value
+//that says if the room light is on/off.
+
+  // Light Sensor pin
+  const int pinLight = A0;
+  void setup(){
+    Serial.begin(9600);}
+  void loop()
+  {
+      // Read the value of the light sensor. The light sensor is an analog sensor.
+      int sensorValue = analogRead(pinLight);
+      
+      // Print sensorValue
+      Serial.println(sensorValue);//Open Serial monitor to see
+      
+  }
---
+++ Initial tests code/Relay/relayTest.ino
@@ -0,0 +1,33 @@
+// This program tests the Grove relay with a Grove Light Sensor
+// If the light sensor reading is above the threshold, the relay is turned ON(closed)
+// If the light sensor reading is below the threshold, the relay is turned OFF(opened)
+
+// Define the pins to which the sensor and relay are attached.
+const int pinLightSensor = A3;
+const int relayPin =  8;
+
+// Defines the light-sensor threshold value( we obtained this value while testing the light sensor)
+int thresholdvalue = 400;
+
+void setup()
+{
+    // Configure the relay's pin for output signals.
+    pinMode(relayPin, OUTPUT);
+}
+
+void loop()
+{
+    // Read the value of the light sensor.
+    int sensorValue = analogRead(pinLightSensor);
+
+    // Activate the realy if the sensor value is above threshold
+    if (sensorValue > thresholdvalue)   
+    {
+        digitalWrite(relayPin, HIGH);
+    }
+    else   
+    {
+        digitalWrite(relayPin, LOW);
+    }
+    delay(10);
+}
