f0c180cbc3e5eb1ef918c16cf065273cc19ee83d
Tomas Borba <tomasborba@gmail.com>
2017-01-27 15:33:22 -0200

    Initiating firmware code
    Basic code structures in place

---
+++ firmware/ASlapAlarmClock.ino
@@ -0,0 +1,23 @@
+// ASlapAlarmClock
+
+//setando as portas correspondetes para cada dispositivo a ser ativado
+int motor = D0;
+int buzzer = D7;
+int led = D1;
+
+void setup()
+{
+
+}
+
+
+void loop()
+{
+
+
+}
+
+void alarmClock()
+{
+
+}
