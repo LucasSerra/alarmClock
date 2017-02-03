0c66af7dc608aebdd94ad84b9f414f80eeecd341
Tomas Borba <tomasborba@gmail.com>
2017-02-03 16:15:13 -0200

    Function to get current time
    Updated firmware to include function to get current time

--- firmware/ASlapAlarmClock.ino
+++ firmware/ASlapAlarmClock.ino
@@ -1,12 +1,30 @@
 // ASlapAlarmClock
 
+#include <SparkTime.h>
+
 //setando as portas correspondetes para cada dispositivo a ser ativado
-int motor = D0;
+int motor = D6;
 int buzzer = D7;
-int led = D1;
+int lightSensor = D3;
+int hourSet;
+int minuteSet;
+bool set = false;
+
+//string currentTime;
+int hourCurrent;
+int minuteCurrent;
 
 void setup()
 {
+
+  pinMode(motor, OUTPUT);
+  pinMode(lightSensor, INPUT);
+
+  //Particle.function("alarm", setAlarm);
+
+  //Particle.syncTime();
+
+  Serial.begin(9600);
 
 }
 
@@ -14,10 +32,35 @@ void setup()
 void loop()
 {
 
+  //if (alarm[SET] && alarm[HOUR] == Time.hour() && alarm[MINUTE] == Time.minute())
+    getTime();
+
 
 }
 
-void alarmClock()
+
+int setAlarm(bool set, int hourSet, int minuteSet)
 {
+/*
+    hourCurrent = hourSet;
+    minuteCurrent = minuteSet;
+    set = true;
+    //EXTRA ligar LED do Spark para confirmar
+    return 1;
+*/
+
+}
+
+void getTime()
+{
+  //time_t t = Time.now();
+  Particle.syncTime();
+  
+  hourCurrent = Time.hour();
+  minuteCurrent = Time.minute();
+  
+  Serial.println(Time.hour());
+
+  
 
 }
